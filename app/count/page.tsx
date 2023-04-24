d87793440530f5baa2740554d7f915f19c33beac
rabire <rabireh@outlook.fr>
2023-04-24 15:34:41 +0200

    feat: 🎸 count page

--- app/count/page.tsx
+++ app/count/page.tsx
@@ -1,7 +1,39 @@
+import Count from "@/components/Count";
 import React from "react";
+import Image from "next/image";
+
+const DATA = [
+  { city: "Dieppe", count: 11190, img: "/dieppe.jpg" },
+  { city: "Marseille", count: 334, img: "/marseille.jpg" },
+  { city: "Berk-sur-mer", count: 2220, img: "/berk.jpg" },
+];
 
 const CountPage = () => {
-  return <main>CountPage</main>;
+  return (
+    <main>
+      <h1 className="text-3xl text-center my-8 mx-auto bg-white p-4 fixed w-full">
+        Cefs volants en temps reel
+      </h1>
+
+      <div className="grid grid-cols-3">
+        {DATA.map((el) => (
+          <div
+            key={el.city}
+            className="flex flex-col items-center justify-center h-screen relative"
+          >
+            <Image
+              src={el.img}
+              alt="bg"
+              fill
+              className="-z-10 object-cover opacity-60"
+            />
+            <h2 className="text-xl font-bold mb-6">{el.city}</h2>
+            <Count count={el.count} />
+          </div>
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default CountPage;
