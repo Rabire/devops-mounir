b29967453840b8c738330e39333e35522a676189
rabire <rabireh@outlook.fr>
2023-04-24 13:53:01 +0200

    feat: 🎸 adding home card component
    this is a component used in the home page.

---
+++ components/Card.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Props = {
+  title: string;
+  text: string;
+};
+
+const Card = ({ title, text }: Props) => {
+  return (
+    <div className="bg-white rounded">
+      <div className="bg-gray-200 w-full h-52" />
+      <div className="p-4">
+        <p className="font-semibold ">{title}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
