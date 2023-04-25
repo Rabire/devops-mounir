c8ac4094206047a475bcfa76cfe1411c19e828cb
YannBonaudo <yannbonaudo@yahoo.fr>
2023-04-25 17:04:30 +0200

    feat(tests): testing getStats api route with jest

--- app/api/stats/route.ts
+++ app/api/stats/route.ts
@@ -1,25 +1,8 @@
+import { generateStats } from "@/utils/stats";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const stats = [
-    {
-      location: "Dieppe",
-      kiteNumber: Math.floor(Math.random() * 20000 + 1),
-      img: "/dieppe.jpg",
-    },
-    {
-      location: "Marseille",
-      kiteNumber: Math.floor(Math.random() * 20000 + 1),
-      img: "/marseille.jpg",
-    },
-    {
-      location: "Berk-sur-mer",
-      kiteNumber: Math.floor(Math.random() * 20000 + 1),
-      img: "/berk.jpg",
-    },
-  ];
-
-  const total = stats.reduce((sum, array) => sum + array.kiteNumber, 0);
+  const stats = generateStats();
 
   return NextResponse.json({ stats, total }, { status: 202 });
 }
--- utils/stats.ts
+++ utils/stats.ts
@@ -1,3 +1,26 @@
+export const generateStats = () => {
+  const stats = [
+    {
+      location: "Dieppe",
+      kiteNumber: Math.floor(Math.random() * 20000 + 1),
+      img: "/dieppe.jpg",
+    },
+    {
+      location: "Marseille",
+      kiteNumber: Math.floor(Math.random() * 20000 + 1),
+      img: "/marseille.jpg",
+    },
+    {
+      location: "Berk-sur-mer",
+      kiteNumber: Math.floor(Math.random() * 20000 + 1),
+      img: "/berk.jpg",
+    },
+  ];
+
+  const total = stats.reduce((sum, array) => sum + array.kiteNumber, 0);
+  return { stats, total };
+};
+
 const getStats = async () => {
   const res = await fetch("/api/stats");
 
