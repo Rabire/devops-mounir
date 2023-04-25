7fcd6b3a6af4dbc5a5697e290bbc3bed7d2b7819
rabire <rabireh@outlook.fr>
2023-04-25 11:24:13 +0200

    config: 🔧 adding eslint airbnb extention and prettier

--- components/Card.tsx
+++ components/Card.tsx
@@ -5,16 +5,14 @@ type Props = {
   text: string;
 };
 
-const Card = ({ title, text }: Props) => {
-  return (
-    <div className="bg-white rounded">
-      <div className="bg-gray-200 w-full h-52" />
-      <div className="p-4">
-        <p className="font-semibold ">{title}</p>
-        <p>{text}</p>
-      </div>
+const Card = ({ title, text }: Props) => (
+  <div className="bg-white rounded">
+    <div className="bg-gray-200 w-full h-52" />
+    <div className="p-4">
+      <p className="font-semibold ">{title}</p>
+      <p>{text}</p>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
