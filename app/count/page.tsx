fbd7c23ffa2d7ffcf84d0b97c2a17de366a717c0
YannBonaudo <yannbonaudo@yahoo.fr>
2023-04-25 11:22:10 +0200

    clean

--- app/count/page.tsx
+++ app/count/page.tsx
@@ -5,19 +5,13 @@ import React from "react";
 import Image from "next/image";
 import useFetchStats from "@/hooks/useFetchStats";
 
-const DATA = [
-  { city: "Dieppe", count: 11190, img: "/dieppe.jpg" },
-  { city: "Marseille", count: 334, img: "/marseille.jpg" },
-  { city: "Berk-sur-mer", count: 2220, img: "/berk.jpg" },
-];
-
 const CountPage = () => {
   const { stats } = useFetchStats(true);
 
   return (
     <main>
       <h1 className="text-3xl text-center my-8 mx-auto bg-white p-4 fixed w-full">
-        Cefs volants en temps reel
+        Total de cerfs-volants en temps réel : {stats?.total}
       </h1>
 
       <div className="grid grid-cols-3">
--- utils/stats.ts
+++ utils/stats.ts
@@ -1,8 +1,7 @@
 export const getStats = async () => {
-    const res = await fetch("/api/stats");
-  
-    if (!res.ok) throw new Error("Failed to get stats");
-  
-    return res.json();
-  };
-  
+  const res = await fetch("/api/stats");
+
+  if (!res.ok) throw new Error("Failed to get stats");
+
+  return res.json();
+};
