1f70b3f8b02c0cbe59498dbaaedbdc4d0944f510
rabire <rabireh@outlook.fr>
2023-04-27 13:59:14 +0200

    feat: ✨ add miss spell

--- components/Card.tsx
+++ components/Card.tsx
@@ -9,7 +9,7 @@ const Card = ({ title, text }: Props) => (
   <div className="bg-white rounded">
     <div className="bg-gray-200 w-full h-52" />
     <div className="p-4">
-      <p className="font-semibold ">{title}</p>
+      <p className="font-semibold">{title}</p>
       <p>{text}</p>
     </div>
   </div>
